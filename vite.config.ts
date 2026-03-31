fc8ced57978628d8f4bf9cc62f548b74cd3ef789
Paul G. <mtesqtsdlc2@gmail.com>
2026-03-31 12:59:23 +0200

    refactor: Simplify Vite build config by removing manual chunking and update API proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -18,38 +18,13 @@ export default defineConfig(({ mode }) => {
         },
         build: {
             sourcemap: false,
-            rollupOptions: {
-                output: {
-                    manualChunks(id) {
-                        if (id.includes('node_modules')) {
-                            // 1. Separar Excalidraw (es el más grande)
-                            if (id.includes('@excalidraw')) return 'vendor-excalidraw';
-
-                            // 2. Separar Mermaid
-                            if (id.includes('mermaid')) return 'vendor-mermaid';
-
-                            // 3. Separar las librerías de grafos (Cytoscape y ELK)
-                            if (id.includes('cytoscape') || id.includes('elkjs')) return 'vendor-graphs';
-
-                            // 4. Separar KaTeX
-                            if (id.includes('katex')) return 'vendor-katex';
-                            if (id.includes('react') || id.includes('jotai') || id.includes('react-router')) {
-                                return 'vendor-framework';
-                            }
-                            // 5. Todo lo demás de React/Bootstrap/etc.
-                            return 'vendor-core';
-                        }
-                    },
-                },
-            },
         },
         server: {
-
             proxy: {
                 '/api': {
                     target: 'https://dd1kekevp7.execute-api.eu-west-1.amazonaws.com',
                     changeOrigin: true,
-                    secure: true,
+                    secure: false,
                 },
             },
         },
