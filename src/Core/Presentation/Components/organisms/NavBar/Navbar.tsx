e24ab56d05f2b142413fe44fa54157a67220cd62
Paul G. <mtesqtsdlc2@gmail.com>
2026-04-14 12:03:34 +0200

    feat: fixing menu on mobile phone

--- src/Core/Presentation/Components/organisms/NavBar/Navbar.tsx
+++ src/Core/Presentation/Components/organisms/NavBar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import './Navbar.scss'
 import { Navbar, Container, Image, Nav } from "react-bootstrap";
+import { useState } from "react";
 
 import logo from "../../../../../Assets/Images/logo-web.png";
 interface NavbarProps {
@@ -10,12 +11,24 @@ interface NavbarProps {
 }
 
 export default function VHNavbar({ isAuthenticated, onLogout, role }: NavbarProps) {
+    const [expanded, setExpanded] = useState(false);
     
     return (
-        <Navbar expand="lg" className="bg-body-tertiary" collapseOnSelect>
+        <Navbar
+            expand="lg"
+            className="bg-body-tertiary"
+            collapseOnSelect
+            expanded={expanded}
+            onToggle={(nextExpanded) => setExpanded(!!nextExpanded)}
+            onSelect={() => setExpanded(false)}
+        >
             <Container>
                 {/* Marca y Logo */}
-                <Navbar.Brand as={Link} to="/">
+                <Navbar.Brand
+                    as={Link}
+                    to="/"
+                    onClick={() => setExpanded(false)}
+                >
                     <Image
                         src={logo}
                         alt="VibraHeka"
@@ -30,25 +43,31 @@ export default function VHNavbar({ isAuthenticated, onLogout, role }: NavbarProp
                 {/* CONTENIDO COLAPSABLE: Debe ser Navbar.Collapse */}
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={NavLink} to="/">Inicio</Nav.Link>
-                        <Nav.Link as={NavLink} to="/terapeutas">Terapeutas</Nav.Link>
+                        <Nav.Link as={NavLink} to="/" onClick={() => setExpanded(false)}>Inicio</Nav.Link>
+                        <Nav.Link as={NavLink} to="/terapeutas" onClick={() => setExpanded(false)}>Terapeutas</Nav.Link>
 
                         {!isAuthenticated && (
                             <>
-                                <Nav.Link as={NavLink} to="/registro">Registrarse</Nav.Link>
-                                <Nav.Link as={NavLink} to="/login">Identificarse</Nav.Link>
+                                <Nav.Link as={NavLink} to="/registro" onClick={() => setExpanded(false)}>Registrarse</Nav.Link>
+                                <Nav.Link as={NavLink} to="/login" onClick={() => setExpanded(false)}>Identificarse</Nav.Link>
                             </>
                         )}
 
                         {(role === 1 && isAuthenticated) && (
-                            <Nav.Link as={NavLink} to="/admin">Admin</Nav.Link>
+                            <Nav.Link as={NavLink} to="/admin" onClick={() => setExpanded(false)}>Admin</Nav.Link>
                         )}
 
                         { isAuthenticated && (
                             <>
-                                <Nav.Link as={NavLink} to="/profile/me">Mi Perfil</Nav.Link>
-                                <Nav.Link as={NavLink} to="/talleres">Talleres</Nav.Link>
-                                <button onClick={onLogout} className="nav-link">
+                                <Nav.Link as={NavLink} to="/profile/me" onClick={() => setExpanded(false)}>Mi Perfil</Nav.Link>
+                                <Nav.Link as={NavLink} to="/talleres" onClick={() => setExpanded(false)}>Talleres</Nav.Link>
+                                <button
+                                    onClick={() => {
+                                        setExpanded(false);
+                                        onLogout();
+                                    }}
+                                    className="nav-link"
+                                >
                                     Cerrar sesión
                                 </button>
                             </>
