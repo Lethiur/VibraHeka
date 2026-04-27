ebb09a230f92df051f7cb8bad629cd89ceb4b477
Pablo G <mtesqtsdlc2@gmail.com>
2026-04-27 21:20:29 +0200

    [VH-68_Verify_account_with_link] : removed redundant validation layers for verification data—because who needs extra bouncers at the party? Streamlined the flow to let the backend do the heavy lifting.
    ──────────────────────────────
    
    Goodbye unnecessary boilerplate! Let's keep things lean and mean.

--- src/Core/Domain/Composition/AuthComposition.ts
+++ src/Core/Domain/Composition/AuthComposition.ts
@@ -3,8 +3,6 @@ import { AuthRepositoryImpl } from "@auth/Data/Repositories/AuthRepositoryImpl";
 import VerifyUserUseCaseImpl from "@auth/Application/UseCases/VerifyUser/VerifyUserUseCaseImpl";
 import LoginUserDataValidator from "@auth/Application/Validators/LoginUserDataValidator";
 import LoginUserUseCase from "@auth/Application/UseCases/LoginUser/LoginUserUseCase";
-import VerificationDataValidator
-    from "@auth/Application/Validators/VerificationDataValidator";
 import RegistrationDataValidator
     from "@auth/Application/Validators/RegistrationDataValidator";
 import RegisterUserUseCase
@@ -25,7 +23,7 @@ const repository = new AuthRepositoryImpl(datasource);
 
 export const registerUserUseCase = new RegisterUserUseCase(repository, new RegistrationDataValidator());
 
-export const verifyUserUseCase = new VerifyUserUseCaseImpl(repository, new VerificationDataValidator());
+export const verifyUserUseCase = new VerifyUserUseCaseImpl(repository);
 
 export const loginUserUseCase = new LoginUserUseCase(repository, new LoginUserDataValidator(), new LocalStorageService());
 
