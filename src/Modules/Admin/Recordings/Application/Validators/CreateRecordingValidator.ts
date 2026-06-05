4d65d44f325bc5d6da484462a1a2ac47fcadc1a2
Pablo G <mtesqtsdlc2@gmail.com>
2026-06-05 11:22:13 +0200

    test

--- src/Modules/Admin/Recordings/Application/Validators/CreateRecordingValidator.ts
+++ src/Modules/Admin/Recordings/Application/Validators/CreateRecordingValidator.ts
@@ -8,7 +8,7 @@ export default class CreateRecordingValidator extends Validator<CreateRecordingE
         this.ruleFor("Name").notNull().notEmpty().withMessage(RecordingsApplicationErrors.NAME_REQUIRED);
         this.ruleFor("Description").notNull().notEmpty().withMessage(RecordingsApplicationErrors.DESCRIPTION_REQUIRED);
         this.ruleFor("Type")
-            .must((value) => value === RecordingType.MEDITACION || value === RecordingType.MASTERCLASS || value === RecordingType.TALLER)
+            .must((value) => value === RecordingType.MEDITACION || value === RecordingType.MASTERCLASS || value === RecordingType.TALLER || value === RecordingType.CONSTELLATION)
             .withMessage(RecordingsApplicationErrors.TYPE_REQUIRED);
         this.ruleFor("File").must((value) => value instanceof File).withMessage(RecordingsApplicationErrors.FILE_REQUIRED);
         this.ruleFor("Price")
